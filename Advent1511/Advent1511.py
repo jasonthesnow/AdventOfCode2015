ee4bfa8f600aed938de0afa9c01f4db88e0be7c6
jasonthesnow <84247656+jasonthesnow@users.noreply.github.com>
2022-08-16 10:20:29 -0600

    Solved Day 11
    Solved part 1 and 2 for day 11, learning new ways to compare strings to string and more complex range arguments

--- Advent1511/Advent1511.py
+++ Advent1511/Advent1511.py
@@ -4,13 +4,58 @@
 # cannot contain i, o, or l
 # must have two pairs of letters that do not overlap (aaa doesn't count, but aaaa would)
 
-puzzle_input = "hxbxwxba"
-temp_input = ""
+password = ['h','x','b','x','x','y','z','z']
+safe_letters = "abcdefghjkmnpqrstuvwxyz"
+counted_up = False
 
-#count up letters
+bad_password = True
 
+has_triple_run = False
+has_double_pair = False
 
+while True:
+    has_triple_run = False
+    has_double_pair = False
+    #Count Up Letters
 
+    counted_up = False
+    for i in range(len(password) - 1, -1, -1):
+        if counted_up:
+            break
+        for j in range(len(safe_letters)):
+            if password[i] == safe_letters[j]:
+                try:
+                    password[i] = safe_letters[j + 1]
+                    counted_up = True
+                    break
+                except IndexError:
+                    password[i] = safe_letters[0]
+    
+    password_string = "".join(password)
+    # print(password_string)
 
+    #Test if password is good
 
+    for i in range(len(safe_letters) - 2):
+        if safe_letters[i:i+3] in password_string:
+            has_triple_run = True
+            break
+    
+    index = list()
+    for i in range(len(safe_letters)):
+        for j in range(len(password)):
+            try:
+                if safe_letters[i] == password[j] and safe_letters[i] == password[j+1]:
+                    index.append(i)
+            except IndexError:
+                pass
+        
+    if len(index) > 1:
+        if index[1] - index[0] > 1:
+            has_double_pair = True
 
+    
+    if has_triple_run and has_double_pair:
+        break
+
+print(password_string)
